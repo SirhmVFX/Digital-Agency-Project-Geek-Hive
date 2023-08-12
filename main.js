20a29c0099d6eedbe8be06da2f596a9db7e83e91
sirhmvfx <sirhmvfx@gmail.com>
2023-08-12 11:32:55 +0100

    finished mobile nav bar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const hamburger = document.querySelector(".hamburger ");
-const navMenu = document.querySelector("nav_menu");
+const navMenu = document.querySelector(".nav_menu");
 
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
